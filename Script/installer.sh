2189c9bc2895f152363c4cc59aeb18b1372b81ca
Divyanshu Kumar <divyanshukumarg@gmail.com>
2018-05-15 13:51:15 +0530

    Updating versions of few packages

--- Script/installer.sh
+++ Script/installer.sh
@@ -345,28 +345,28 @@ echo "Installing ant"
 sudo fink install ant
 clear
 
-echo "Installing gettext-bin-0.19.3-1"
-sudo fink install gettext-bin-0.19.3-1
+echo "Installing gettext-bin-0.19.8.1-2"
+sudo fink install gettext-bin-0.19.8.1-2
 clear
 
-echo "Installing gettext-tools-0.19.3-1"
-sudo fink install gettext-tools-0.19.3-1
+echo "Installing gettext-tools-0.19.8.1-2"
+sudo fink install gettext-tools-0.19.8.1-2
 clear
 
-echo "Installing arpack-ng-3.1.5-3"
-sudo fink install arpack-ng-3.1.5-3
+echo "Installing arpack-ng-3.4.0-1"
+sudo fink install arpack-ng-3.4.0-1
 clear
 
-echo "Installing pkgconfig-0.28-1"
-sudo fink install pkgconfig-0.28-1
+echo "Installing pkgconfig-0.28-2"
+sudo fink install pkgconfig-0.28-2
 clear
 
-echo "Installing libcurl4-7.42.0-1+10.8"
-sudo fink install libcurl4-7.42.0-1+10.8
+echo "Installing libcurl4-7.57.0-1"
+sudo fink install libcurl4-7.57.0-1
 clear
 
-echo "Installing libmatio2-1.5.2-2"
-sudo fink install libmatio2-1.5.2-2
+echo "Installing libmatio2-1.5.3-1"
+sudo fink install libmatio2-1.5.3-1
 clear
 
 
