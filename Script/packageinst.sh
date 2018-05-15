8776d9fbd8a76c61d19fe7d3d940b5bbed7e9b98
Divyanshu Kumar <divyanshukumarg@gmail.com>
2018-05-15 21:47:23 +0530

    trying to check if a package is installed

--- Script/packageinst.sh
+++ Script/packageinst.sh
@@ -2,56 +2,61 @@
 echo "Installing dependencies with fink"
 . /sw/bin/init.sh
 echo " Installing  suitesparse"
-sudo fink install suitesparse
+s1=$(sudo fink install suitesparse)
+echo "$s1"
+if [ s1 == "No packages to install." ] ; then
+  echo "suitesparse is already installed"
+else
+  echo "not installed"
+fi
+
+#echo "Installing fftw3"
+#sudo fink install fftw3
 
 
-echo "Installing fftw3 "
-sudo fink install fftw3
+#echo "Installing libmatio2"
+#sudo fink install libmatio2
 
 
-echo "Installing libmatio2"
-sudo fink install libmatio2
+#echo "Installing libmatio2-shlibs"
+#sudo fink install libmatio2-shlibs
 
 
-echo "Installing libmatio2-shlibs"
-sudo fink install libmatio2-shlibs
+#echo "Installing hdf5.8"
+#sudo fink install hdf5.8
 
 
-echo "Installing hdf5.8"
-sudo fink install hdf5.8
+#echo "Installing hdf5.8-shlibs"
+#sudo fink install hdf5.8-shlibs
 
 
-echo "Installing hdf5.8-shlibs"
-sudo fink install hdf5.8-shlibs
+#echo " Installing libpcre1"
+#sudo fink install libpcre1
 
 
-echo " Installing libpcre1"
-sudo fink install libpcre1
+#echo "Installing ant"
+#sudo fink install ant
 
 
-echo "Installing ant"
-sudo fink install ant
+#echo "Installing gettext-bin"
+#sudo fink install gettext-bin
 
 
-echo "Installing gettext-bin-0.19.8.1-2"
-sudo fink install gettext-bin-0.19.8.1-2
+#echo "Installing gettext-tools"
+#sudo fink install gettext-tools
 
 
-echo "Installing gettext-tools-0.19.8.1-2"
-sudo fink install gettext-tools-0.19.8.1-2
+#echo "Installing arpack-ng"
+#sudo fink install arpack-ng
 
 
-echo "Installing arpack-ng-3.4.0-1"
-sudo fink install arpack-ng-3.4.0-1
+#echo "Installing pkgconfig"
+#sudo fink install pkgconfig
 
 
-echo "Installing pkgconfig-0.28-2"
-sudo fink install pkgconfig-0.28-2
+#echo "Installing libcurl4"
+#sudo fink install libcurl4
 
 
-echo "Installing libcurl4-7.57.0-1"
-sudo fink install libcurl4-7.57.0-1
-
-
-echo "Installing libmatio2-1.5.3-1"
-sudo fink install libmatio2-1.5.3-1
+#echo "Installing libmatio2"
+#sudo fink install libmatio2
