551a01010d86c210812aaff8e06ad32113641e41
Divyanshu Kumar <divyanshukumarg@gmail.com>
2018-05-16 17:45:21 +0530

    Updating minor changes

--- Script/installer.sh
+++ Script/installer.sh
@@ -4,6 +4,12 @@
 # shellcheck disable=SC1091
 # shellcheck disable=SC1117
 
+# Checking if user has root access
+if [[ $EUID -ne 0 ]]; then
+   echo "You must be root to do this."
+   exit 0
+fi
+
 # Config
 OSXVersion="$(sw_vers -productVersion | cut -f -2 -d .)"
 DarwinVersion="$(uname -r | cut -d. -f1)"
@@ -312,9 +318,11 @@ fi
 fink selfupdate
 clear
 
-echo "Installing dependencies with fink"
+echo "Installing Scilab dependencies with fink"
+
 . /sw/bin/init.sh
 
+# An array containing list of packages to install
 declare -a packages=("suitesparse"
                       "fftw3"
                       "libmatio2"
@@ -328,33 +336,52 @@ declare -a packages=("suitesparse"
                       "pkgconfig"
                       "libcurl4"
                       "libmatio2")
+
+# Installing each package one by one
 for i in "${packages[@]}"
 do
    clear
    echo " Installing  $i"
-   sudo fink install $i
+   fink install $i
 done
+
+# A variable to count number of unistalled packages
 count=0
+
+# A variable to store list of installed packages
 installed=""
+
+# A variable to store list of uninstalled packages
 ninstalled=""
+
+# A variable to store a list of all packages installed through fink
 listi=$(fink list -i)
+
 
 for i in "${packages[@]}"
 do
+# checking if listi contains a package specified
    if [ -z "${listi##*$i*}" ] ; then
      installed="$installed \n $i"
    else
+     # Increasing count, if its uninstalled
      count=`expr $count + 1`
      ninstalled="$ninstalled \n $i"
    fi
 done
 
+# Clearing screen
 clear
-echo -e "List of installed packages : $installed \n\n"
+echo  "List of installed packages : $installed \n\n"
+
+# If count > 0 then there is a package which is not installed, i.e. Not ready to compile yet
 if [ $count -gt 0 ] ; then
-  echo -e "Following packages were not installed: $ninstalled\n\n Please install them manually"
+  echo  "Following packages were not installed: $ninstalled\n\n Please install them manually"
 else
-  echo  -e "Congratulations everything is perfectly setup to compile Scilab\n\n"
+  # All packages are installed and ready to compile
+  echo   "Congratulations everything is perfectly setup to compile Scilab\n\n"
 fi
+
+
 
 exit 0
---
+++ Script/scilabDependencies.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+echo "Installing Scilab dependencies with fink"
+. /sw/bin/init.sh
+
+# Checking if user has root access
+if [[ $EUID -ne 0 ]]; then
+   echo "You must be root to do this."
+   exit 0
+fi
+
+# An array containing list of packages to install
+declare -a packages=("suitesparse"
+                      "fftw3"
+                      "libmatio2"
+                      "libmatio2-shlibs"
+                      "hdf5.9" "hdf5.9-shlibs"
+                      "libpcre1"
+                      "ant"
+                      "gettext-bin"
+                      "gettext-tools"
+                      "arpack-ng"
+                      "pkgconfig"
+                      "libcurl4"
+                      "libmatio2")
+
+# Installing each package one by one
+for i in "${packages[@]}"
+do
+   clear
+   echo " Installing  $i"
+   fink install $i
+done
+
+# A variable to count number of unistalled packages
+count=0
+
+# A variable to store list of installed packages
+installed=""
+
+# A variable to store list of uninstalled packages
+ninstalled=""
+
+# A variable to store a list of all packages installed through fink
+listi=$(fink list -i)
+
+
+for i in "${packages[@]}"
+do
+# checking if listi contains a package specified
+   if [ -z "${listi##*$i*}" ] ; then
+     installed="$installed \n $i"
+   else
+     # Increasing count, if its uninstalled
+     count=`expr $count + 1`
+     ninstalled="$ninstalled \n $i"
+   fi
+done
+
+# Clearing screen
+clear
+echo  "List of installed packages : $installed \n\n"
+
+# If count > 0 then there is a package which is not installed, i.e. Not ready to compile yet
+if [ $count -gt 0 ] ; then
+  echo  "Following packages were not installed: $ninstalled\n\n Please install them manually"
+else
+  # All packages are installed and ready to compile
+  echo   "Congratulations everything is perfectly setup to compile Scilab\n\n"
+fi
