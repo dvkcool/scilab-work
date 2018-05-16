5398884b57f93869169ed3c5d4e195399ce7b20b
Divyanshu Kumar <divyanshukumarg@gmail.com>
2018-05-16 12:15:30 +0530

    Checking if possible to get list of installed packages

--- Script/packageinst.sh
+++ Script/packageinst.sh
@@ -1,62 +1,41 @@
 
 echo "Installing dependencies with fink"
 . /sw/bin/init.sh
-echo " Installing  suitesparse"
-s1=$(sudo fink install suitesparse)
-echo "$s1"
-if [ "$s1" = "No packages to install." ] ; then
-  echo "suitesparse is already installed"
-else
-  echo "not installed"
-fi
 
-#echo "Installing fftw3"
-#sudo fink install fftw3
+declare -a packages=("suitesparse"
+                      "fftw3"
+                      "libmatio2"
+                      "libmatio2-shlibs"
+                      "hdf5.8" "hdf5.8-shlibs"
+                      "libpcre1"
+                      "ant"
+                      "gettext-bin"
+                      "gettext-tools"
+                      "arpack-ng"
+                      "pkgconfig"
+                      "libcurl4"
+                      "libmatio2")
+#for i in "${packages[@]}"
+#do
+#   clear
+#   echo " Installing  $i"
+#   sudo fink install $i -y
+#done
+c = 0
+installed=""
+ninstalled=""
+listi=$(fink list -i)
+for i in "${packages[@]}"
+do
+   if [ "$listi" = *"$i"* ] ; then
+     $installed = $installed + "\n $i"
+   else
+     c = `expr $c + 1`
+     $ninstalled = $ninstalled + "\n $i"
+   fi
+done
 
-
-#echo "Installing libmatio2"
-#sudo fink install libmatio2
-
-
-#echo "Installing libmatio2-shlibs"
-#sudo fink install libmatio2-shlibs
-
-
-#echo "Installing hdf5.8"
-#sudo fink install hdf5.8
-
-
-#echo "Installing hdf5.8-shlibs"
-#sudo fink install hdf5.8-shlibs
-
-
-#echo " Installing libpcre1"
-#sudo fink install libpcre1
-
-
-#echo "Installing ant"
-#sudo fink install ant
-
-
-#echo "Installing gettext-bin"
-#sudo fink install gettext-bin
-
-
-#echo "Installing gettext-tools"
-#sudo fink install gettext-tools
-
-
-#echo "Installing arpack-ng"
-#sudo fink install arpack-ng
-
-
-#echo "Installing pkgconfig"
-#sudo fink install pkgconfig
-
-
-#echo "Installing libcurl4"
-#sudo fink install libcurl4
-
-
-#echo "Installing libmatio2"
-#sudo fink install libmatio2
+clear
+echo "List of installed packages : $installed"
+if [ $c -ge 0 ] ; then
+  echo "Following packages were not installed"
